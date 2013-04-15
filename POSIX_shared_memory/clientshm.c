81d7fd1845610506bb49e852b456193769d39e5a
Jeshwanth Kumar N K <jeshkumar555@gmail.com>
2013-04-15 19:07:20 +0530

    Update clientshm.c
    Changed variable name

--- POSIX_shared_memory/clientshm.c
+++ POSIX_shared_memory/clientshm.c
@@ -14,8 +14,8 @@
 /* message structure for messages in the shared segment */
 
 struct shared_data {
-    int voltage;
-    int current;
+    int var1;
+    int var2;
 };
 
 void signal_callback_handler(int signum)
@@ -65,8 +65,8 @@ int main(int argc, char *argv[]) {
 
     while(1)
     {
-    printf("The voltage is %d \n",shared_msg->voltage);
-    printf("The current is %d \n",shared_msg->current);
+    printf("The var1 is %d \n",shared_msg->var1);
+    printf("The var2 is %d \n",shared_msg->var2);
     sleep(2);
     }
 
